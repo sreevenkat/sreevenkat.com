15ca6a10c66415e498bfc09c695ff3737477ade8
Sree Venkat <gsvetr@gmail.com>
2023-07-22 16:30:41 +0530

    update: home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,18 +35,12 @@ const ProjectListComponent = () => (
 
 const ProfileSection = () => (
 	<Stack direction={{ base: 'column', md: 'row' }} spacing={8} py={10}>
-		<Image
-			borderRadius="full"
-			boxSize="130px"
-			src={'https://media-exp1.licdn.com/dms/image/C5103AQFxRzJJZITVhg/profile-displayphoto-shrink_200_200/0/1570330526418?e=1662595200&v=beta&t=2-tyySrQ49_6Tx4SCiQprL_A8BXxKKyPE9d2xT85xU4'}
-			alt={'Sree Venkat'}
-		/>
 		<Stack justifyContent={'center'}>
 			<Heading fontSize="5xl" fontWeight="extrabold">
 				Hi, I&apos;m Sree Venkat Ganapathi 👋
 			</Heading>
 			<Text fontSize={'md'}>
-				Senior software engineer at Refyne.
+				Software engineer at Refyne.
 			</Text>
 		</Stack>
 	</Stack>
